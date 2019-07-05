f9afcadebec27ecd6d7aba758f641ef5b5466fdc
Thomas Nieto <38873752+tnieto88@users.noreply.github.com>
2019-07-04 21:10:36 -0500

    Completed the module

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -45,7 +45,7 @@ namespace GradeBook.UserInterfaces
             
             if (type == "standard")
             {
-                gradeBook = new BaseGradeBook(name);
+                gradeBook = new StandardGradeBook(name);
             }
             else if (type == "ranked")
             {
